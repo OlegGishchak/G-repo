8c7df1846bd43b7e6ad34b4e39f19dacc0d9d30b
Gishchak <celonshark@gmail.com>
2018-09-13 17:56:07 +0300

    start commit

---
+++ LinqPro/LinqPro/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LinqPro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Tom", "Dick", "Harry", "Mary", "Jay" };
+            IEnumerable<string> query = from n in names
+                                      select Regex.Replace(n, "[aeiou]", "")
+
+                                      into noVowel
+                                      where noVowel.Length > 2
+                                      orderby noVowel
+                                      select noVowel; 
+            foreach (var item in query)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
